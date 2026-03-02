c38f962f35ec9821c242475af55b8eb6b5784c94
FelinGG <piterson050406@gmail.com>
2026-03-02 12:06:47 +0100

    new changes

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,15 @@
+// Obsługa głównego menu bocznego
 function toggleMenu() {
-    document.getElementById('sidebar').classList.toggle('active');
-    document.querySelector('.overlay').classList.toggle('active');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.querySelector('.overlay');
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
+// Obsługa rozwijanej listy Top 5 Lektur
+function toggleDropdown() {
+    const content = document.getElementById('top5-menu');
+    const arrow = document.getElementById('dropdown-arrow');
+    content.classList.toggle('show');
+    arrow.classList.toggle('rotate');
 }
